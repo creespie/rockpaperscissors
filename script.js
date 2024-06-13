46dd48ac2c9016edbdd9899f602672d8cee86ca2
creespie <150524904+creespie@users.noreply.github.com>
2024-06-13 22:58:38 +0200

    update with 5 rounds game move everything into playround to fix scope

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function round(){
-    if(humanChoice == computerChoice){
-        return draw();
-    }else if((humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "papaer")){
-        return win();
-    }else{
-    return loss();
+
+
+//create the starting scores
+let computerScore = 0;
+let humanScore = 0;
+
+//function to play the round
+function playRound(){
+    let humanChoice = getHumanImput();
+    let computerChoice = getComputerChoice();
+    
+    //functions for human loss, win or draw
+    function win(){
+        humanScore = humanScore + 1;
+        console.log("You won!"+ humanChoice + " beats " + computerChoice);
+        console.log("your score is " + humanScore);
     }
-}
-//get the computer choice with random generation of numbers
-function getComputerChoice(){
+    
+    function loss(){
+        computerScore = computerScore + 1;
+        console.log("You lost!"+ computerChoice + " beats " + humanChoice);
+        console.log("computer score is "+ computerScore);
+    }
+    
+    function draw(){
+        console.log("It's a draw!")
+    }
+
+
+    //get the computer choice with random generation of numbers
+    function getComputerChoice(){
     let x = Math.floor(Math.random() * 3);
     let choice;
     if(x === 0){
@@ -19,10 +39,10 @@ function getComputerChoice(){
        choice = "scissors";
     };
     return choice;
-}
+    }
 
-//get the human imput via the prompt command
-function getHumanImput(){
+    //get the human imput via the prompt command
+    function getHumanImput(){
     let choice = prompt("rock, paper or scissors?");
     let fixedchoice = choice.toLowerCase();
     let human;
@@ -38,31 +58,9 @@ function getHumanImput(){
         return error;
     };
     return human;
-}
+    }
 
-//create the starting scores
-let computerScore = 0;
-let humanScore = 0;
-let humanChoice = getHumanImput();
-let computerChoice = getComputerChoice();
-
-//functions for human loss, win or draw
-function win(){
-    humanScore = humanScore + 1;
-    console.log("You won!"+ humanChoice + " beats " + computerChoice)
-}
-
-function loss(){
-    computerScore = computerScore + 1;
-    console.log("You lost!"+ computerChoice + " beats " + humanChoice)
-}
-
-function draw(){
-    console.log("It's a draw!")
-}
-
-//function to play the round
-function round(){
+    //check who won
     if(humanChoice === computerChoice){
         return draw();
     }else if((humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "papaer")){
@@ -71,4 +69,11 @@ function round(){
     return loss();
     }
 }
-console.log(round());
+
+//play the game
+function playGame(){
+    for (let i = 1; i <= 5; i++){
+        playRound();
+}
+}
+console.log(playGame())
